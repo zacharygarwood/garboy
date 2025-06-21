70909cb98e497d58631e448c6e67b362bfd6965f
Zachary Garwood <garwoodzachary@gmail.com>
2025-06-21 18:01:31 -0500

    feat: Swapped to test ROM 5 and passed it right away :)

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 func main() {
-	cartridge := NewCartridge("./test_roms/04-op r,imm.gb", 0x2000)
+	cartridge := NewCartridge("./test_roms/05-op rp.gb", 0x2000)
 	ppu := NewPPU()
 	cpu := NewCPU(cartridge, ppu)
 
 	cpu.SkipBootROM()
 
-	maxCycles := 15_000_000
+	maxCycles := 20_000_000
 	totalCycles := 0
 
 	for totalCycles < maxCycles {
